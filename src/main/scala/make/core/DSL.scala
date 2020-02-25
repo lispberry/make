848aab650eed68d36b3cf47f4835c9f2dd8222e0
Ilya Volkov <ilya@lispberry.com>
2020-02-25 19:23:49 +0500

    Update tests

--- src/main/scala/make/core/DSL.scala
+++ src/main/scala/make/core/DSL.scala
@@ -7,7 +7,25 @@ class DSL(val owner: Recipe) {
       // TODO: either make sure nonexistent tasks are permitted or create a specific Task subclass.
       // TODO: UGLY, UGLY, UGLY
       if (!owner.hasTask(dep)) {
-        val task = new FileTask(owner, dep, { _ => () })
+        val task = new PhonyTask(owner, dep, { _ => () })
+        owner.addTask(task)
+      }
+
+      task.addDependency(dep)
+    }
+    owner.addTask(task)
+
+    this
+  }
+
+  def task(name: String)(deps: String*)(command: Command): DSL = {
+    // TODO: copy-paste
+    val task = new PhonyTask(owner, name, command)
+    for (dep <- deps) {
+      // TODO: either make sure nonexistent tasks are permitted or create a specific Task subclass.
+      // TODO: UGLY, UGLY, UGLY
+      if (!owner.hasTask(dep)) {
+        val task = new PhonyTask(owner, dep, { _ => () })
         owner.addTask(task)
       }
 
--- src/test/scala/make/core/SequentialStrategySuite.scala
+++ src/test/scala/make/core/SequentialStrategySuite.scala
@@ -2,27 +2,58 @@ package make.core
 
 import make.MakeSuite
 
+import scala.collection.mutable.ArrayBuffer
+
 class SequentialStrategySuite extends MakeSuite {
-  test("execution") {
+  test("simple execution") {
     import DSL._
 
-    // TODO: terrible test
-    var nth = 0
+    val list = new ArrayBuffer[String]
     val rec = recipe {
-      _.file("a")("b", "c") {
-        _ => assert(nth < 2); nth += 1
-      }.file("c")("b", "d") {
-        _ => assert(nth < 2); nth += 1
-      }.file("b")() {
-        _ => assert(nth >= 2); nth += 1
-      }.file("c")() {
-        _ => assert(nth >= 2); nth += 1
-      }.file("d")() {
-        _ => assert(nth >= 2); nth += 1
+      _.task("a")("b", "c") {
+        _ => list += "a"
+      }.task("b")() {
+        _ => list += "b"
+      }.task("c")() {
+        _ => list += "c"
       }
     }
 
     val strategy = new SequentialStrategy
     strategy.execute(rec)
+
+    assert(list(0) == "b")
+    assert(list(1) == "c")
+    assert(list(2) == "a")
+  }
+
+  test("nested execution") {
+    import DSL._
+
+    val list = new ArrayBuffer[String]
+    val rec = recipe {
+      _.task("a")("b", "c") {
+        _ => list += "a"
+      }.task("b")("c", "d") {
+        _ => list += "b"
+      }.task("c")() {
+        _ => list += "c"
+      }.task("d")() {
+        _ => list += "d"
+      }
+    }
+
+    val strategy = new SequentialStrategy
+    strategy.execute(rec)
+
+    def before(list: ArrayBuffer[String], x: String, y: String): Boolean = {
+      list.indexOf(x) < list.indexOf(y) && list.indexOf(x) != -1 && list.indexOf(y) != -1
+    }
+
+    assert(before(list, "b", "a"))
+    assert(before(list, "c", "a"))
+    assert(before(list, "d", "a"))
+    assert(before(list, "c", "b"))
+    assert(before(list, "d", "b"))
   }
 }
